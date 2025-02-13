4e8d55ad71e92d18f88846aad9c568844dc87351
SofyaPav <sofyapavlova12@gmail.com>
2025-02-13 23:45:48 +0300

    делаю лаб 1

---
+++ scripts/practice_1/lab1_e.py
@@ -0,0 +1,25 @@
+# pyside6-designer
+# cd scripts\practice_1\ui
+# PySide6-uic lab1_e_form.ui -o lab1_e_form.py
+
+
+from PySide6 import QtWidgets
+
+from ui.lab1_e_form import Ui_MainWindow
+
+
+class Window(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication()
+
+    window = Window()
+    window.show()
+
+    app.exec()
